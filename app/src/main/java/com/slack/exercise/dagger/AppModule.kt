53d640605fe9d07ab2f33afa0dce017f0e3f6f20
Brad Spurgin <brad.spurgin@charter.com>
2024-01-03 17:28:42 -0700

    cleanup

--- app/src/main/java/com/slack/exercise/dagger/AppModule.kt
+++ app/src/main/java/com/slack/exercise/dagger/AppModule.kt
@@ -1,17 +1,11 @@
 package com.slack.exercise.dagger
 
-import android.app.Application
-import android.content.Context
-import com.slack.exercise.App
 import com.slack.exercise.api.SlackApi
 import com.slack.exercise.api.SlackApiImpl
 import com.slack.exercise.dataprovider.UserSearchResultDataProvider
 import com.slack.exercise.dataprovider.UserSearchResultDataProviderImpl
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
-
 
 /**
  * Module to setup Application scoped instances that require providers.
@@ -25,12 +19,4 @@ abstract class AppModule {
 
     @Binds
     abstract fun provideSlackApi(apiImpl: SlackApiImpl): SlackApi
-
-    companion object {
-        @Provides
-        @Singleton
-        fun provideApplicationContext(application: App): Context {
-            return application.applicationContext
-        }
-    }
 }
