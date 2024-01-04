ba973e777781e8d4f6749997e9359b627142872e
Brad Spurgin <brad.spurgin@charter.com>
2024-01-04 16:25:47 -0700

    Migrated RecyclerViewAdapter in UserSearchAdapter.kt to extend ListAdapter

--- app/src/main/java/com/slack/exercise/ui/usersearch/UserSearchAdapter.kt
+++ app/src/main/java/com/slack/exercise/ui/usersearch/UserSearchAdapter.kt
@@ -4,21 +4,19 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.slack.exercise.databinding.ItemUserSearchBinding
 import com.slack.exercise.model.UserSearchResult
 
 /**
- * Adapter for the list of [UserSearchResult].
+ * ListAdapter for the list of [UserSearchResult].
  */
-class UserSearchAdapter : RecyclerView.Adapter<UserSearchAdapter.UserSearchViewHolder>() {
-    private var userSearchResults: List<UserSearchResult> = emptyList()
-
-    fun setResults(results: Set<UserSearchResult>) {
-        userSearchResults = results.toList()
-        notifyDataSetChanged()
-    }
+class UserSearchAdapter : ListAdapter<UserSearchResult, UserSearchAdapter.UserSearchViewHolder>(
+    UserSearchResultsDiffCallback()
+) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserSearchViewHolder {
         val binding: ItemUserSearchBinding =
@@ -26,12 +24,8 @@ class UserSearchAdapter : RecyclerView.Adapter<UserSearchAdapter.UserSearchViewH
         return UserSearchViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return userSearchResults.size
-    }
-
     override fun onBindViewHolder(holder: UserSearchViewHolder, position: Int) {
-        val user = userSearchResults[position]
+        val user = getItem(position)
         holder.username.text = user.username
         holder.displayName.text = user.displayName
         Glide.with(holder.avatar.context).load(user.avatarUrl).into(holder.avatar)
@@ -42,5 +36,15 @@ class UserSearchAdapter : RecyclerView.Adapter<UserSearchAdapter.UserSearchViewH
         val username: TextView = binding.username
         val avatar: ImageView = binding.imageViewAvatar
         val displayName: TextView = binding.displayName
+    }
+
+    private class UserSearchResultsDiffCallback : DiffUtil.ItemCallback<UserSearchResult>() {
+        override fun areItemsTheSame(oldItem: UserSearchResult, newItem: UserSearchResult): Boolean {
+            return oldItem.username == newItem.username
+        }
+
+        override fun areContentsTheSame(oldItem: UserSearchResult, newItem: UserSearchResult): Boolean {
+            return oldItem == newItem
+        }
     }
 }
